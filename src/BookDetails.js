fcd98b3f535edec2e6b04234bcdcc8948b4f00c0
Poitier Stringer <stringerpoitier@gmail.com>
2021-03-29 16:47:25 -0700

    able to fetch json data from books api

--- src/BookDetails.js
+++ src/BookDetails.js
@@ -1,7 +1,14 @@
+import useFetch from './useFetch';
+
 const BookDetails = () => {
+    const {data : book, error, isPending } = useFetch('https://openlibrary.org/api/books?bibkeys=ISBN:0385472579&format=json');
+    console.log(book);
     return (
         <div className='book-details'>
-            
+            <p>
+                {isPending && 'Book Data is Loading...'}
+                {error && 'Sorry. Something went wrong.' && error }
+            </p>
         </div>
     );
 }
